663d79055666103e1188e61567420741e623f67c
sumityadavpw <139611100+sumityadavpw@users.noreply.github.com>
2023-07-30 14:20:45 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "sumit@google.com"
+var accountPassord = "123456"
+// accountCity = jaipur   
+let accountState; 
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+
+accountEmail ="amit@sk.com"
+accountPassord = "525212"
+accountCity = "Delhi"
+
+console.log([accountId, accountEmail, accountPassord, accountCity, accountState,])
+
+
+/*prefer not to use var because of issue in block 
+ scope and functional scope */ 
