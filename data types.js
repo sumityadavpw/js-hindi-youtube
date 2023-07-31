32b9fe12f871a15ffa61a239e793045978f9a268
sumityadavpw <139611100+sumityadavpw@users.noreply.github.com>
2023-07-31 06:34:33 +0000

    conversion operation for java script

---
+++ data types.js
@@ -0,0 +1,20 @@
+//"use strict"; //treat all js code as newer version
+
+//alert (3+3) // we are using node js not browser
+
+let name = "sumit"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean = true/false
+// null = standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
