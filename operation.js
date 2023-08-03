9a4be11514739729646c4fe63d1106d3e4518e48
sumityadavpw <139611100+sumityadavpw@users.noreply.github.com>
2023-08-03 12:18:37 +0000

    comperasation

---
+++ operation.js
@@ -0,0 +1,71 @@
+// let score = "55"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1
+// false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 
+// 0 => false;
+// "" => false
+// "sumit" => true
+
+
+// let someNumber = 33
+
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*********Operation********/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**12);
+console.log(2/2);
+console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = "sumit"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2); //not the correct way
+// console.log(1 + "2"); //not the correct way
+// console.log("1" + 2 + 2); //not the correct way
+// console.log(1 + 2 + "2"); //not the correct way
+
+// console.log(3 + 4 * 5 % 3);// not the correct way
+
+// console.log(+true); //not the correct way
+//  console.log(""); //not the correct way
+
+// let num1, num2, num3  //not the correct way
+
+// num1 = num2 = num3 = 2 +2  //not the correct way
+
+let gameCounetr = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+ 
