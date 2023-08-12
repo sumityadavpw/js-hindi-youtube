d0a9b0d21bf52f492f3923f5f4c39450fa1dc2aa
sumityadavpw <139611100+sumityadavpw@users.noreply.github.com>
2023-08-12 19:46:27 +0000

    stack and heap

---
+++ stackheap.js
@@ -0,0 +1,21 @@
+//-----StacK(primitive), Heap(non-primitive)
+
+let myYoutubeName = "sumityadavdotcom"
+
+let anothername = myYoutubeName
+anothername = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userone = {
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userone
+
+userTwo.email = "sumityadavdotcom"
+
+console.log(userone.email);
+console.log(userTwo.email);
