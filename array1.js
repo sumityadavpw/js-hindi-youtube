a24f7e73d6b593da789d0b0c9a796f96851e826f
sumityadavpw <139611100+sumityadavpw@users.noreply.github.com>
2023-08-19 19:53:49 +0000

    Array1

---
+++ array1.js
@@ -0,0 +1,45 @@
+//+++++ Array ++++++//
+
+const myArray = [0, 1, 2, 3, 4, 5]
+// const myHeros = ["shaktiman", "Ironman"]
+
+// const myArray2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArray[2])
+
+
+//+++ Array Methods +++//
+
+//  myArray.push(6)
+//  myArray.push(7)
+//  myArray.pop()
+    
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(5));
+
+
+// const newArray = myArray.join()
+
+//  console.log(myArray);
+//  console.log(newArray);
+
+
+
+//+++++ Slice, Splice Methods ++++//
+
+console.log("A " , myArray);
+
+const mynum1 = myArray.slice(1, 3)
+
+console.log(mynum1)
+console.log(" B ", myArray)
+
+const mynum2 = myArray.splice(1, 3)
+console.log("c", myArray)
+console.log(mynum2)
+
+
+
+
